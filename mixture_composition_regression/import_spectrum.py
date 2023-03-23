d9b372f748cae88fe9e4dee05ff5b619687c77a6
Ian Billinge <ianbillinge@Ians-MacBook-Pro-9.local>
2023-03-23 12:48:27 -0400

    Added import_spectrum.py

---
+++ mixture_composition_regression/import_spectrum.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+def clean_data(filename):
+    """
+    Open and clean a csv file of data.
+
+    Parameters
+    ----------
+    filename : string
+        Filepath to file.
+        The refractive index of medium of origin and destination medium.
+
+    Returns
+    -------
+    df : DataFrame
+        Pandas DataFrame containing the x-ray absorption and
+
+    Examples
+    --------
+
+    """
+
+    df = pd.read_csv(filename)
+    df = df.dropna(axis=1, how="all")
+
+    row_cutoff = (
+        df.isna().idxmax("index").where(df.isna().any("index"))
+    )  # find a cutoff row by finding where there are any nan values
+    row_cutoff = int(row_cutoff.mode()) - 1
+
+    df = df.iloc[1:row_cutoff]  # cut off the row at that point
+    df = df.astype("float")
+    return df
