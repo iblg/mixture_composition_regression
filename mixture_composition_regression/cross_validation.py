befa00e34b01e588affdb834c38a9388943edbd4
Ian Billinge <ian.billinge@gmail.com>
2023-04-19 13:54:26 -0400

    Removed some unnecessary imports and created regression_playground.py

--- mixture_composition_regression/cross_validation.py
+++ mixture_composition_regression/cross_validation.py
@@ -1,26 +1,18 @@
-import matplotlib.pyplot as plt
 import sklearn
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.svm import SVR
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.gaussian_process.kernels import ExpSineSquared
+
 
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_absolute_percentage_error
-# from ib_mpl_stylesheet.ib_mpl_stylesheet import ib_mpl_style
+
+
+import mixture_composition_regression.mixture
+from mixture_composition_regression.preprocessor_pipeline import get_Xy
+from mixture_composition_regression.preprocessor_pipeline import plot_metric
 
 import numpy as np
 
-import mixture_composition_regression.mixture
-from mixture_composition_regression.tests.import_training_set import import_training_set
-from mixture_composition_regression.preprocessor_pipeline import *
-
-
-# They need to be able to try different models, different params within those models, and different wavelength ranges
 
 def cv_on_model_and_wavelength(m: mixture_composition_regression.mixture.Mixture,
                                nwindows: list,
@@ -127,102 +119,3 @@ def get_window_list(start: float, end: float, nwindows: list = None, width: floa
 
     windows = np.concatenate((starts, ends), axis=1)
     return windows
-
-
-def main():
-    water_dipa_nacl, water_dipa, water_nacl = import_training_set()
-
-    lbounds = [800, 3200]  # set global bounds on wavelength
-
-    mix_test = water_dipa_nacl.filter({'nacl': [10 ** -5, 1], 'dipa': [10 ** -5, 1]})
-    nwindows = [1, 10, 30]
-    sc = 'neg_mean_absolute_error'
-    random_state = 42
-    tts_size = 0.25
-    ycol = 0  # water
-    # ycol = 1  # dipa
-    # ycol = 2  # salt
-    # metric = mean_absolute_percentage_error
-    # metric_label = 'Mean abs fractional err'
-    metric = mean_absolute_error
-    metric_label = 'MAE'
-
-    mix_train = water_dipa + water_nacl
-
-    # m.plot_by(idx=2, savefig='plotby', alpha=1, logy=True, cmap_name='viridis', spect_bounds=lbounds, stylesheet=None)
-
-    ridge = GridSearchCV(
-        Ridge(), {'alpha': np.logspace(-7, 7, 14)}, scoring=sc, cv=5
-    )
-
-    kr = GridSearchCV(
-        KernelRidge(),
-        param_grid={'kernel': ["rbf", 'linear'],
-                    "alpha": np.logspace(-5, 5, 11),
-                    "gamma": np.logspace(-5, 5, 11)},
-        scoring=sc,
-    )
-
-    svr = GridSearchCV(
-        SVR(),
-        {'kernel': ['linear', 'rbf'],
-         'gamma': ['scale', 'auto'],
-         'epsilon': np.logspace(-5, 5, 10)
-         },
-        scoring=sc,
-    )
-
-    knnr = GridSearchCV(
-        KNeighborsRegressor(), {'n_neighbors': 5 + np.arange(5)}, scoring=sc
-    )
-
-    mlp = GridSearchCV(
-        MLPRegressor(solver='lbfgs', max_iter=400),
-        {'hidden_layer_sizes': [10, 50, 100]},
-        scoring=sc
-    )
-
-    cv_models = [
-        ridge,
-        kr,
-        svr,
-        knnr,
-        # mlp,
-    ]
-    print('hi')
-
-
-    viable_models, best_model = cv_on_model_and_wavelength(
-        mix_train,
-        nwindows, cv_models, ycol=ycol,
-                                                           test_data=mix_test,
-                                                           tts_test_size=tts_size,
-                                                           tts_random_state=random_state,
-                                                           tolerance=5E-4,
-                                                           metric=metric,
-                                                           metric_label=metric_label,
-                                                           l_bounds=lbounds,
-                                                           plot_comparison=True,
-                                                           plot_comparison_savefile='./plots/axes_train'
-                                                           )
-
-    viable_models, best_model = cv_on_model_and_wavelength(water_dipa_nacl,
-                                                           nwindows,
-                                                           cv_models,
-                                                           ycol=ycol,
-                                                           tts_test_size=tts_size,
-                                                           tts_random_state=random_state,
-                                                           tolerance=5E-4,
-                                                           metric=metric,
-                                                           metric_label=metric_label,
-                                                           l_bounds=lbounds,
-                                                           plot_comparison=True,
-                                                           plot_comparison_savefile='./plots/normal_tts'
-                                                           )
-
-
-    return
-
-
-if __name__ == '__main__':
-    main()
---
+++ mixture_composition_regression/regression_playground.py
@@ -0,0 +1,113 @@
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.linear_model import Ridge
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
+
+import numpy as np
+
+from mixture_composition_regression.tests.import_training_set import import_training_set
+from mixture_composition_regression.cross_validation import cv_on_model_and_wavelength
+
+
+def main():
+    water_dipa_nacl, water_dipa, water_nacl = import_training_set()
+
+    lbounds = [800, 3200]  # set global bounds on wavelength
+
+    mix_test = water_dipa_nacl.filter({'nacl': [10 ** -5, 1], 'dipa': [10 ** -5, 1]})
+    nwindows = [1]
+    sc = 'neg_mean_absolute_error'
+    random_state = 42
+    tts_size = 0.25
+    ycol = 0  # water
+    # ycol = 1  # dipa
+    # ycol = 2  # salt
+    # metric = mean_absolute_percentage_error
+    # metric_label = 'Mean abs fractional err'
+    metric = mean_absolute_error
+    metric_label = 'MAE'
+
+    mix_train = water_dipa + water_nacl
+
+    # m.plot_by(idx=2, savefig='plotby', alpha=1, logy=True, cmap_name='viridis', spect_bounds=lbounds, stylesheet=None)
+
+    ridge = GridSearchCV(
+        Ridge(), {'alpha': np.logspace(-7, 7, 14)}, scoring=sc, cv=5
+    )
+
+    kr = GridSearchCV(
+        KernelRidge(),
+        param_grid={'kernel': ["rbf", 'linear'],
+                    "alpha": np.logspace(-5, 5, 11),
+                    "gamma": np.logspace(-5, 5, 11)},
+        scoring=sc,
+    )
+
+    svr = GridSearchCV(
+        SVR(),
+        {'kernel': ['linear', 'rbf'],
+         'gamma': ['scale', 'auto'],
+         'epsilon': np.logspace(-5, 5, 10)
+         },
+        scoring=sc,
+    )
+
+    knnr = GridSearchCV(
+        KNeighborsRegressor(), {'n_neighbors': 5 + np.arange(5)}, scoring=sc
+    )
+
+    mlp = GridSearchCV(
+        MLPRegressor(solver='lbfgs', max_iter=400),
+        {'hidden_layer_sizes': [10, 50, 100]},
+        scoring=sc
+    )
+
+    cv_models = [
+        ridge,
+        # kr,
+        # svr,
+        # knnr,
+        # mlp,
+    ]
+
+    viable_models, best_model = cv_on_model_and_wavelength(
+        mix_train,
+        nwindows, cv_models, ycol=ycol,
+        test_data=mix_test,
+        tts_test_size=tts_size,
+        tts_random_state=random_state,
+        tolerance=5E-4,
+        metric=metric,
+        metric_label=metric_label,
+        l_bounds=lbounds,
+        plot_comparison=True,
+        plot_comparison_savefile='./plots/axes_train'
+    )
+    #
+
+    # viable_models, best_model = cv_on_model_and_wavelength(water_dipa_nacl,
+    #                                                        nwindows,
+    #                                                        cv_models,
+    #                                                        ycol=ycol,
+    #                                                        tts_test_size=tts_size,
+    #                                                        tts_random_state=random_state,
+    #                                                        tolerance=5E-4,
+    #                                                        metric=metric,
+    #                                                        metric_label=metric_label,
+    #                                                        l_bounds=lbounds,
+    #                                                        plot_comparison=True,
+    #                                                        plot_comparison_savefile='./plots/normal_tts'
+    #                                                        )
+
+    return
+
+
+if __name__ == '__main__':
+    main()
